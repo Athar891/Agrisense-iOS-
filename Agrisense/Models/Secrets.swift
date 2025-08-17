54dba4b52e6abf22c7acb1671f5f9b8ec42f83b0
Athar Reza <athar@Athars-MacBook-Air.local>
2025-08-17 20:33:33 +0530

    Inital Commit

---
+++ Agrisense/Models/Secrets.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum Secrets {
+    static let openWeatherAPIKey = "a82212783c4282ff62113cda5724c72e"
+
+    // Cloudinary Credentials - Replace with your actual credentials
+    static let cloudinaryCloudName = "derhnhko0"
+    static let cloudinaryApiKey = "766535777996872"
+    static let cloudinaryApiSecret = "2mLl4-90XqAUs0r1irajzbQw6s"
+    static let cloudinaryUploadPreset = "crop_images"
+}
